c329f4d55f27639c728d3115ace3b722e027dfbe
Gastón Alvarez <gaston_a96@hotmail.com>
2025-07-30 22:45:35 -0300

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Malla-Universitaria/',
+  base: '/Traduccion-Usach-Malla/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
